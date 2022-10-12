84fec282b64bb3f6a333c1eb4b77933894a1b8b3
Artour <nekron08@gmail.com>
2022-10-12 15:40:47 +0300

    'add:split files'

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Technologies  from './technologies'
 const App = function () {
   return (
     <div className="App">
+        {/*<Header/>*/}
         <Header/>
       <h1>My React App</h1>
-       <Technologies/>
+        <Technologies/>
     </div>
   );
 }
-const Technologies = function (){
-    return(
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    )
-}
-const Header = function (){
-    return(
-        <div className='header' >
-            <h2>I am header</h2>
-            <a href="https://create-react-app.dev/">react</a>
-        </div>
-    )
-}
+
+
 
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header = function (){
+    return(
+        <div className='header' >
+            <h2>I am header</h2>
+            <a href="https://create-react-app.dev/">react</a>
+        </div>
+    )
+}
+export default Header
